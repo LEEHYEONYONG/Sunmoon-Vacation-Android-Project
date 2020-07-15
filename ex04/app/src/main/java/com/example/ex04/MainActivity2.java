2a6f7d1c6cda15ecc9de59dc043e16e1f6cf4c42
LEEHYEONYONG <dusqhfk36@naver.com>
2020-07-15 09:52:33 +0900

    init ex04

--- ex04/app/src/main/java/com/example/ex04/MainActivity2.java
+++ ex04/app/src/main/java/com/example/ex04/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.ex04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        LinearLayout page1 = findViewById(R.id.page1);
+        LinearLayout page2 = findViewById(R.id.page2);
+        LinearLayout page3 = findViewById(R.id.page3);
+
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(page1.getVisibility()==View.VISIBLE)
+            }
+        });
+
+        Button btn2 =findViewById(R.id.btn2);
+        btn2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button btn3=findViewById(R.id.btn3);
+        btn3.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- ex04/app/src/main/java/com/example/ex04/SubActivity.java
+++ ex04/app/src/main/java/com/example/ex04/SubActivity.java
@@ -3,12 +3,45 @@ package com.example.ex04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
+        setContentView(R.layout.sub);
+
+        Button btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView image = findViewById(R.id.image);
+                TextView name =findViewById(R.id.name);
+
+
+
+
+                if(image.getVisibility()==View.VISIBLE){
+                    //사라지기
+                    image.setVisibility(View.INVISIBLE);
+                }else{
+                    //보이기
+                    image.setVisibility(View.VISIBLE);
+                }
+
+
+                if(name.getVisibility()==View.VISIBLE){
+                    name.setVisibility(View.INVISIBLE);
+                }else{
+                    name.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
     }
 }
