042ef07f43c51984f3fd22cdc9dc5fe9727d0efa
LEEHYEONYONG <dusqhfk36@naver.com>
2020-07-15 11:30:12 +0900

    modify: ex07 imageView

--- ex07/app/src/main/java/com/example/ex07/MainActivity2.java
+++ ex07/app/src/main/java/com/example/ex07/MainActivity2.java
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     LinearLayout page1,page2,page3;
+    EditText text1;
+    ImageView image;
+    String str="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,8 @@ public class MainActivity2 extends AppCompatActivity {
         page1=findViewById(R.id.page1);
         page2=findViewById(R.id.page2);
         page3=findViewById(R.id.page3);
+        text1=findViewById(R.id.text1);
+        image=findViewById(R.id.image);
     }
 
     //버튼클릭 메서드
@@ -32,6 +40,23 @@ public class MainActivity2 extends AppCompatActivity {
                 break;
             case R.id.btn3:
                 page3.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn4:
+                page1.setVisibility(View.VISIBLE);
+                str=text1.getText().toString();
+                Toast.makeText(MainActivity2.this,str,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imagebtn1:
+                page2.setVisibility(View.VISIBLE);
+                image.setImageResource(R.drawable.car1);
+                break;
+            case R.id.imagebtn2:
+                page2.setVisibility(View.VISIBLE);
+                image.setImageResource(R.drawable.car2);
+                break;
+            case R.id.imagebtn3:
+                page2.setVisibility(View.VISIBLE);
+                image.setImageResource(R.drawable.car3);
                 break;
         }
     }
