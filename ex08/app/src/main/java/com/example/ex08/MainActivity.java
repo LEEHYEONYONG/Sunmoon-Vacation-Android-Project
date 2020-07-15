afc97a9f0de49c9b0c4c60939490910b8dd720ae
LEEHYEONYONG <dusqhfk36@naver.com>
2020-07-15 15:11:44 +0900

    Add: ex08

--- ex08/app/src/main/java/com/example/ex08/MainActivity.java
+++ ex08/app/src/main/java/com/example/ex08/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ex08;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        ImageView image=findViewById(R.id.image);
+        switch(item.getItemId()){
+            case R.id.item1:
+                image.setImageResource(R.drawable.item1);
+                break;
+            case R.id.item2:
+                image.setImageResource(R.drawable.item2);
+                break;
+            case R.id.item3:
+                image.setImageResource(R.drawable.item4);
+                break;
+            case R.id.item4:
+                image.setImageResource(R.drawable.item3);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
