569466727b56db4c7bd216c0411cd5edd36399cb
LEEHYEONYONG <dusqhfk36@naver.com>
2020-07-20 17:52:21 +0900

    modify ex15

--- ex15/app/src/main/java/com/example/ex15/MainActivity.java
+++ ex15/app/src/main/java/com/example/ex15/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 //startActivity(intent);
 
